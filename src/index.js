b22c3b98efbbe7bcafa5a93a548f368f03bbac9e
tobnys <tobnystrom@gmail.com>
2017-07-20 16:53:39 +0200

    added 'routing' and animations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// https://openweathermap.org/current
-// https://openweathermap.org/api
-
-
-
 (function(){
     const YT_API = "https://www.googleapis.com/youtube/v3/search";
+    $(".current").fadeToggle(1, function(){
+
+    });
 
     $(".js-main-form-btn").on("click", function(e){
         e.preventDefault();
@@ -12,6 +10,34 @@
         console.log(searchValue);
         initiateAPI(searchValue);
     });
+
+    $(".js-current-weather").on("click", function(e){
+        e.preventDefault();
+        $(".home").fadeOut(400, function(){
+            renderCurrentWeather();
+        });
+    });
+
+    function renderCurrentWeather()
+    {
+        $(".current").fadeIn(400, function(){
+
+        });
+    }
+
+    $(".js-home").on("click", function(e){
+        e.preventDefault();
+        $(".current").fadeOut(400, function(){
+            $(".home").fadeIn(400, function(){
+                renderMainPage();
+            });
+        });
+    });
+
+    function renderMainPage()
+    {
+        $(".current-weather").fadeOut(400);
+    }
 
 
     function initiateAPI(searchTerm)
