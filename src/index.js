e985d53f50ae480b0b4b9e08302546fb2704388d
tobnys <tobnystrom@gmail.com>
2017-07-20 21:06:02 +0200

    prototype complete, basic API functionality added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 (function(){
     const YT_API = "https://www.googleapis.com/youtube/v3/search";
-    $(".current").fadeToggle(1, function(){
+    var runThroughCurrent = 0;
+    var runOnceSearch = false;
 
-    });
+    $(".current").fadeToggle(1);
+    $(".searchInfo").fadeToggle(1);
+    
+
     $(".js-main-form-btn").on("click", function(e){
         e.preventDefault();
         var searchValue = $(".js-main-input").val();
-        console.log(searchValue);
-        initiateAPI(searchValue);
+        initiateSearch(searchValue);
+        $(".js-main-form-btn").trigger("reset");
+    });
+
+    $(".js-main-input").keypress(function(e){
+        var key = e.which;
+        if(key === 13)
+        {
+            e.preventDefault();
+            $(".js-main-form-btn").click();
+        }
     });
 
     $(".js-current-weather").on("click", function(e){
         e.preventDefault();
         $(".home").fadeOut(400, function(){
             renderCurrentWeather();
+            initiateAPI("london");
+            initiateAPI("paris");
+            initiateAPI("stockholm");
         });
     });
 
@@ -22,6 +38,88 @@
         $(".current").fadeIn(400, function(){
 
         });
+    }
+
+    function initiateSearch(q)
+    {
+        $.ajax({
+            method: "GET",
+            url: config.API,
+            data: {
+                APPID: config.API_KEY,
+                q: q
+            }
+        })
+        .done(function(result){
+            console.log("API call complete");
+            if(runOnceSearch === false)
+            {
+                renderSearchPage(result);
+                console.log(q);
+                console.log(result);
+                runOnceSearch = true;
+            }
+        });       
+    }
+
+    function renderSearchPage(api)
+    {
+        $(".searchInfo").fadeIn(400, function(){
+
+        });
+
+        var temp = Math.floor(api.main.temp - 273.15);
+
+        $("#search-t").text(api.name);
+        $("#search-de").append(api.weather[0].description);
+        $("#search-te").append(temp + "&#8451;");
+        $("#search-hu").append(api.main.humidity);
+        $("#search-pr").append(api.main.pressure);
+        $("#search-wi").append(api.wind.speed + " m/s");
+    }
+
+    function initiateAPI(q)
+    {
+        var target;
+        if(q === "london")
+        {
+            target = "#w1";
+        }
+        else if(q === "paris")
+        {
+            target = "#w2";
+        }
+        else if(q === "stockholm")
+        {
+            target = "#w3";
+        }
+        $.ajax({
+            method: "GET",
+            url: config.API,
+            data: {
+                APPID: config.API_KEY,
+                q: q
+            }
+        })
+        .done(function(result){
+            console.log("API call complete");
+            if(runThroughCurrent < 3)
+            {
+                renderCurrentWeatherPage(result, target);
+                runThroughCurrent++;
+            }
+        });
+    }
+
+    function renderCurrentWeatherPage(api, target)
+    {
+        var temp = Math.floor(api.main.temp - 273.15);
+        $(target + "-t").text(api.name);
+        $(target + "-de").append(api.weather[0].description);
+        $(target + "-te").append(temp + "&#8451;");
+        $(target + "-hu").append(api.main.humidity);
+        $(target + "-pr").append(api.main.pressure);
+        $(target + "-wi").append(api.wind.speed + " m/s");
     }
 
     $(".js-home").on("click", function(e){
@@ -36,12 +134,6 @@
     function renderMainPage()
     {
         $(".current-weather").fadeOut(400);
-    }
-
-
-    function initiateAPI(searchTerm)
-    {
-        console.log("Initiating API.");
     }
 
     function renderHTML(result)
